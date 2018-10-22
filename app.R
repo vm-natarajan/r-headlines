c610e107c967ec9750033452d47100f56a38f2d2
Vm Natarajan <veera@Manikandans-MacBook-Air.local>
2018-10-21 19:14:59 -0500

    first commit

--- app.R
+++ app.R
@@ -10,11 +10,34 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- fluidPage(tags$head(
+  tagList(
+    suppressDependencies("bootstrap"),
+    tags$link(
+      rel="stylesheet",
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css",
+      integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy",
+      crossorigin="anonymous"
+    ),
+    tags$script(
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js",
+      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q",
+      crossorigin="anonymous"
+    ),
+    tags$script(
+      src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js",
+      integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4",
+      crossorigin="anonymous"
+    )
+  )),
+  #tags$link(rel = "stylesheet", type = "text/css", href = "bootstrap.css")),
   htmlOutput('op')
-)
+  )
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  data <- read.csv(file = 'stories.csv',stringsAsFactors = FALSE)
+  hd <- data$title;
+  refurls <- data$url
   size <- length(hd);
   rows <- vector("list",(size/3))   
   output$op <- renderUI({
@@ -24,7 +47,9 @@ server <- function(input, output) {
       if(index == 0){
         index <- 3;
       }
-      row[[index]] <- list( column(4,h3(paste0('TOI : ',hd[x]))));
+      
+      card <- tags$div(class = 'card h-100', tags$div(class = 'card-body', tags$h5(class = 'card-title','Business'), tags$p(class = 'card-text',hd[x]),tags$a(href = refurls[x],class="btn btn-primary")))
+      row[[index]] <- list( column(4, card ,class = 'container p-1'));
       if(x%%3 == 0){
         rows[[x%/%3]] <- list(fluidRow(row));
       }
