6b40aa10a096551a72f7a3a27f27fd30db9f8697
Natarajan <Veeramanikandan.Natarajan@directenergy.com>
2018-10-25 16:54:26 -0500

    With graph

--- app.R
+++ app.R
@@ -8,7 +8,11 @@
 #
 
 library(shiny)
-
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(tidytext)
+library(magick)
 # Define UI for application that draws a histogram
 ui <- fluidPage(tags$head(
   tagList(
@@ -34,8 +38,8 @@ ui <- fluidPage(tags$head(
       content="width=device-width, initial-scale=1.0"
     )
   )),
-  #tags$link(rel = "stylesheet", type = "text/css", href = "bootstrap.css")),
-  htmlOutput('op')
+  htmlOutput('op'),
+  plotOutput('plot')
 )
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -49,21 +53,24 @@ server <- function(input, output) {
     row <- vector("list",3)
     for(x in c(1:size)) {
       
-      text <- as.character(content[[x]]) %>% strsplit('\n')
-      
-      words <- data_frame(text = text[[1]]) %>% filter(text != '') %>% unnest_tokens(words,text)
-      
+      text <- unlist(as.character(content[[x]]) %>% strsplit('\n'))
+      data <- data_frame(text = text) %>% filter(text!='') %>% mutate(line = c(1:length(text))) %>% unnest_tokens(word,text) %>% inner_join(afinn,by = 'word') %>% group_by(line) %>% summarise(sum = sum(score)) %>% mutate( index = 1,color = ifelse(sum > 0,'green','red')) 
+      words <- data_frame(text = text) %>% filter(text != '') %>% unnest_tokens(words,text)
       time <- floor( nrow(words)/ 200 )
-      
       index <- x%%3
       
+      ggplot(data = data,aes(x = line,y = index , fill=color)) + geom_bar(stat="identity") + scale_fill_manual( values = c("green" = "mediumseagreen", "red" = "indianred")) + theme_void()  + theme(legend.position = "none")
+      fileName <- paste0('www/','grp','_',x,'.png');
+      ggsave(file = fileName);
+      image <- image_trim(image_read(path = fileName));
+      image <- image_crop(image = image, "3071x150");
+      image_write(path = fileName,image = image);
       if(index == 0){
         index <- 3;
       }
       
-      card <- tags$div(class = 'card h-100',style = 'border-radius: 10px;', tags$div(class = 'card-body', tags$h5(class = 'card-title',hd[x]), tags$p(class = 'card-text',paste0(nchar(content[[x]])),' - ',time),tags$a(href = refurls[x],tags$img(src='toi.png',class = 'rounded')) ))
+      card <- tags$div(class = 'card h-100',style = 'border-radius: 10px;', tags$div(class = 'card-body', tags$h5(class = 'card-title',hd[x]), tags$img(src = paste0('grp','_',x,'.png'),class="img-fluid"),tags$div(tags$a(href = refurls[x],tags$img(src='toi.png',class = 'rounded')),img(src='green.png',class = 'rounded' )   )))
       row[[index]] <- tags$div(class = 'col-sm col-xs-12 container p-1',card)
-      #list( column(4, card ,class = 'container p-1'));
       if(x%%3 == 0){
         rows[[x%/%3]] <- list(fluidRow(row));
       }
